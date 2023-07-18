3a3085d0ca230f4158d7311ae8c82987f20eab21
Josh Fischer <josh@joshfischer.io>
2023-07-18 09:24:04 -0500

    update props

--- projects/iridium-ui/src/environments/environment.prod.ts
+++ projects/iridium-ui/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: false,
+  iridium: {
+    domain: 'CONDUCT_DOMAIN_GOES_HERE',
+    redirectUri: 'REDIRECT_URI_GOES_HERE',
+    successfulAuthDestination: '/dashboard',
+    clientId: 'CLIENT_ID_GOES_HERE',
+    errorPath: '/error'
+  }
 };
